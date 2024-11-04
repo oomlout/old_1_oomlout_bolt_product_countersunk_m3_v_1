1deb16d31428efd66200b4dcf8af6eb01fcc5e19
Oom Lout <aaron@oomlout.com>
2024-11-04 17:11:53 +0000

    working

--- oomp_create_parts_project_this.py
+++ oomp_create_parts_project_this.py
@@ -25,38 +25,73 @@ def load_parts(**kwargs):
         
         # label
         if True:
+            project_name = "oomlout_bolt_product_prototyping_tin_hardware_screw_countersunk_m3_black_hex_head_version_1"
+            base_github = f"https://github.com/oomlout/{project_name}/blob/main"
+            base_oomlout = f"C:\\gh\\{project_name}"
+
             part_details = copy.deepcopy(part_base)
             part_details["description_main"] = "label"
-            part_details["description_extra"] = "working_label_front_print"
+            part_details["description_extra"] = "label_front"
             part_details["name_short"] = "m3_countersunk_tin_label_front"
+
+            file_name = "working_label_front_print.pdf"
+            directory = "release/print"
+
+            part_details["file_name"] = file_name
+            part_details["link_github"] = f"{base_github}/{directory}/{file_name}"
+            part_details["file_name_full_oomlout"] = f"{base_oomlout}/{directory}/{file_name}"
             parts.append(part_details)
 
             part_details = copy.deepcopy(part_base)
             part_details["description_main"] = "label"
-            part_details["description_extra"] = "working_label_inside_print"
+            part_details["description_extra"] = "label_inside"
             part_details["name_short"] = "m3_countersunk_tin_label_inside"
+
+            file_name = "working_label_inside_print.pdf"
+            directory = directory
+
+            part_details["file_name"] = file_name
+            part_details["link_github"] = f"{base_github}/{directory}/{file_name}"
+            part_details["file_name_full_oomlout"] = f"{base_oomlout}/{directory}/{file_name}"
+
             parts.append(part_details)
 
 
             part_details = copy.deepcopy(part_base)
             part_details["description_main"] = "label"
-            part_details["description_extra"] = "working_label_serial_number_print"
+            part_details["description_extra"] = "label_serial"
             part_details["name_short"] = "m3_countersunk_tin_label_serial_number"
+
             parts.append(part_details)
 
         #three d print
         if True:
             part_details = copy.deepcopy(part_base)
-            part_details["description_main"] = "three_d_print"
-            part_details["description_extra"] = "tin_spacer_t4066"
+            part_details["description_main"] = "tdpb"
+            part_details["description_extra"] = "spacer"
             part_details["name_short"] = "prototyping_tin_spacer_t4066"
+            
+            file_name = "tin_spacer_quantity_1.stl"
+            directory = "release\three_d_printer_files\hinged_tin_version"
+
+            part_details["file_name"] = file_name
+            part_details["link_github"] = f"{base_github}/{directory}/{file_name}"
+            part_details["file_name_full_oomlout"] = f"{base_oomlout}/{directory}/{file_name}"
 
             parts.append(part_details)
 
             part_details = copy.deepcopy(part_base)
-            part_details["description_main"] = "three_d_print"
-            part_details["description_extra"] = "tray_4_width_2.5_height_18_mm_depth"
+            part_details["description_main"] = "tdpb"
+            part_details["description_extra"] = "tray42d518"
             part_details["name_short"] = "prototyping_tin_tray_4_width_2.5_height_18_mm_depth"
+
+            file_name = "tray_4_width_2.5_height_18_mm_depth_quantity_4.stl"
+            directory = directory
+
+            part_details["file_name"] = file_name
+            part_details["link_github"] = f"{base_github}/{directory}/{file_name}"
+            part_details["file_name_full_oomlout"] = f"{base_oomlout}/{directory}/{file_name}"
+
             parts.append(part_details)
 
             
